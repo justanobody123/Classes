7befac02359b29c1f6f770be8ec3564e00980213
Кристина Волосова <justanobody017527@gmail.com>
2024-07-01 16:26:30 +0300

    Classwork DONE

--- Encapsulation/Program.cs
+++ Encapsulation/Program.cs
@@ -10,6 +10,61 @@ namespace Encapsulation
 	{
 		static void Main(string[] args)
 		{
-		}
+			Point A = new Point();
+			A.ToString();
+			//A.SetX(2);
+			//A.SetY(3);
+   //         Console.WriteLine($"X = {A.GetX()}\tY = {A.GetY()}");
+			A.X = 22;
+			A.Y = 133;
+            Console.WriteLine($"X = {A.X}\tY = {A.Y}");
+        }
+	}
+	struct Point
+	{
+		//double x;
+		//double y;
+		//public double X
+		//{
+		//	get { return x; } 
+		//	set 
+		//	{
+		//		value = value < 0 ? 0 : value;
+		//		value = value > 100 ? 100: value;
+		//		x = value;
+		//	} 
+		//}
+		//public double Y
+		//{
+		//	get => y;//Вместо return
+		//	set { y = value; }
+		//}
+		//public double GetX()
+		//{
+		//	return x;
+		//}
+		//public double GetY() 
+		//{
+		//	return y;
+		//}
+		//public void SetX(double x) 
+		//{
+		//	this.x = x;
+		//}
+		//public void SetY (double y)
+		//{
+		//	this.y = y;
+		//}
+		public double X {  get; set; }
+		public double Y { get; set; }
+       // public double Z { get; set; }
+		//private double z;
+
+		//public double Z
+		//{
+		//	get { return z; }
+		//	set { z = value; }
+		//}
+
 	}
 }
